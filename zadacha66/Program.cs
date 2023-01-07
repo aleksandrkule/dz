618ad943760cf999eb4ed7447e16e915efb4de13
alex <alex160609@rambler.ru>
2023-01-07 19:14:45 +0400

    задачи к уроку 9

---
+++ zadacha66/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int sum(int m, int n)
+{
+    if (m == n)
+      return  n ;
+    return sum (m, n - 1) + n;
+}
+
+Console.Write("Введите число: ");
+ int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(sum(m, n));
---
+++ zadacha68/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+
+
+  int accerman(int  m, int  n)
+{
+    if (m == 0)
+        return 1 + n ;
+    if  (n == 0 && m > 0)
+        return accerman (m - 1, 1);
+    else
+        return (accerman(m - 1, accerman(m, n - 1)));
+}
+
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(accerman (m, n));
